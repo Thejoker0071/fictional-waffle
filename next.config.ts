8805f147b25fefa2852056ded2057414ba41d53b
Thejoker0071 <cedric0071717@gmail.com>
2025-08-25 20:50:05 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,28 @@
+import type {NextConfig} from 'next';
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.TURBOPACK, // Disable PWA in Turbopack mode
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withPWA(nextConfig);
